c2e186cbce53cb1ca18ccef55d438376cd267c35
Sofia Socorro <socor512224@gmail.com>
2026-03-30 23:28:00 +0000

    basic

---
+++ main.py
@@ -0,0 +1,7 @@
+git clone https://github.com/sofiasocorro/elite102-banking-app.git
+cd elite102-banking-app
+
+print('hello')
+git add main.py
+git commit -m "Adding my first Python file"
+git push origin main
